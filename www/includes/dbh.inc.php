daec2dea092c0bb53b9a4c50f928c0d5700e6f6b
ax666raf <achraf.baha.eddine.bennacer@ensia.edu.dz>
2024-12-17 19:16:37 +0100

    "commit"

---
+++ www/includes/dbh.inc.php
@@ -0,0 +1,13 @@
+<?php
+//this file is used for connecting to the database
+
+$dsn = "mysql:host=localhost;dbname=gym-match";
+$dbusername = "root";
+$dbpassword = "";
+
+try {
+    $pdo = new PDO($dsn,$dbusername,$dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "connection failed: " . $e->getMessage();
+}
