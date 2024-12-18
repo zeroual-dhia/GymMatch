e4f8e71e33d8ed4004dda9e3ab01e23237ba7c19
ax666raf <achraf.baha.eddine.bennacer@ensia.edu.dz>
2024-12-18 12:25:57 +0100

    hi

--- signup.php
+++ signup.php
@@ -26,7 +26,7 @@ try {
 
     $pdo=null;
     $stmt=null;
-    header("Location: ../pages/login.php");
+    header("Location:index.php");
     die();
 } catch (PDOException $e) {
     die("query failed: " . e->getMessage());
@@ -35,5 +35,5 @@ try {
 }
 else{
 
-    header("Location: ../pages/login.php");//user did not enter any data so nothing happens
+    header("Location:index.php");//user did not enter any data so nothing happens
 }
