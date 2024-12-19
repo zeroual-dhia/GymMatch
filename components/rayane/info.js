698ed103ddaf433aba0b1f3dc3f1b390b69c0066
dhia <dhiaaeddine.zeroual@ensia.edu.dz>
2024-12-19 14:50:09 +0100

    commit

--- components/rayane/info.js
+++
@@ -1,6 +0,0 @@
-
-document.querySelectorAll(".action").forEach(element => {
-    element.addEventListener('click', () => {
-        window.location.href = "/components/dhiaa/html/payment.html"; // Relative path to payment.html
-    });
-});
